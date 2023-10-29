0e4e1109ac0ba47a360022b36f42987098540e63
Kovács Dávid <kovacsdavid2013@gmail.com>
2023-10-29 16:04:47 +0100

    Added action to show submenus to amin menu buttons. Each submenu has a button to return to main menu.

--- NHF/src/ButtonActions.java
+++ NHF/src/ButtonActions.java
@@ -1,4 +1,6 @@
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,13 +8,47 @@ public class ButtonActions {
 	public static ActionListener newGameListener = new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-
+			JFrame gameFrame = new JFrame("Malom");
+			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			GUI.frame.setVisible(false);
+			gameFrame.setVisible(true);
+			gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			gameFrame.setSize(screenSize);
+			JButton backToMainMenuButton = new JButton("Visszalépés a főmenübe");
+			Dimension buttonSize = new Dimension(200,50);
+			panel.add(backToMainMenuButton);
+			gameFrame.add(panel);
+			ActionListener listener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					gameFrame.setVisible(false);
+					GUI.frame.setVisible(true);
+				}
+			};
+			backToMainMenuButton.addActionListener(listener);
 		}
 	};
 	public static ActionListener statisticsListener = new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-
+			JFrame statisticsFrame = new JFrame("Malom");
+			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			GUI.frame.setVisible(false);
+			statisticsFrame.setVisible(true);
+			statisticsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			statisticsFrame.setSize(screenSize);
+			JButton backToMainMenuButton = new JButton("Visszalépés a főmenübe");
+			Dimension buttonSize = new Dimension(200,50);
+			panel.add(backToMainMenuButton);
+			statisticsFrame.add(panel);
+			ActionListener listener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					statisticsFrame.setVisible(false);
+					GUI.frame.setVisible(true);
+				}
+			};
+			backToMainMenuButton.addActionListener(listener);
 		}
 	};
 	public static ActionListener exitListener = new ActionListener() {
--- NHF/src/GUI.java
+++ NHF/src/GUI.java
@@ -2,13 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GUI extends ButtonActions {
+	public static JFrame frame = new JFrame("Malom");
 	public GUI() {
-		JFrame frame = new JFrame("Malom");
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
 		frame.setSize(screenSize);
 
 		JPanel panel = new JPanel(new GridBagLayout());
@@ -28,28 +28,20 @@ public class GUI extends ButtonActions {
 		statisticsButton.addActionListener(statisticsListener);
 		exitButton.addActionListener(exitListener);
 
-
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		panel.add(newGameButton, gbc);
-
 		gbc.gridy = 1;
 		panel.add(Box.createRigidArea(new Dimension(0, 10)), gbc); // Add vertical spacing
-
 		gbc.gridy = 2;
 		panel.add(statisticsButton, gbc);
-
 		gbc.gridy = 3;
-		panel.add(Box.createRigidArea(new Dimension(0, 10)), gbc); // Add vertical spacing
-
+		panel.add(Box.createRigidArea(new Dimension(0, 10)), gbc);
 		gbc.gridy = 4;
 		panel.add(exitButton, gbc);
 
-
 		frame.setLocationRelativeTo(null);
-
 		frame.add(panel);
-
 		frame.setVisible(true);
 	}
 }
